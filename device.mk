72eb0cde98e834b0a48346cbfb266633978158f3
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-02-06 16:31:26 +0800

    raphael: Initial crypto support
    * Very initial, should be completed in later commits.

--- device.mk
+++ device.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Crypto
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
